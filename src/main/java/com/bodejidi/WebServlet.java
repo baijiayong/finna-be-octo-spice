259cd2fb628b59385f3b7702d4bafc06a338b9d3
baijiayong <jiayongbai@gmail.com>
2014-03-21 22:19:42 +0800

    新增列出member的功

--- src/main/java/com/bodejidi/WebServlet.java
+++ src/main/java/com/bodejidi/WebServlet.java
@@ -9,13 +9,70 @@ public class WebServlet extends HttpServlet
 {
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,java.io.IOException 
   {
-    String firstName = req.getParameter("first_name");
-    String lastName = req.getParameter("last_name");
+    Connection conn = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+    try
+    {
+      Class.forName("com.mysql.jdbc.Driver").newInstance();
+    } 
+    catch(Exception ex)
+    {
+      //ignore
+    }
     
     try
     {
-      
-    } 
+      conn = DriverManager.getConnection("jdbc:mysql://localhost/test?"
+                                            + "user=root"
+                                            + "&password=");
+      stmt = conn.createStatement();
+      String sql = "SELECT * FROM member";
+      System.out.println("SQL: " + sql);
+      rs = stmt.executeQuery(sql);
+      resp.getWriter().println("<html><head><title>Member List</title></head><body><h1>Member List<h1><table border=\"1\"><tr><td>ID</td><td>Name</td></tr>\n");
+      while(rs.next()) {
+                Long id = rs.getLong("id");
+                String firstName = rs.getString("first_name");
+                String lastName = rs.getString("last_name");
+                resp.getWriter().println("<tr><td>" + id + "</td><td>" + firstName + " " + lastName + "</td></tr>\n");
+            }
+      resp.getWriter().println("</table>");
+      resp.getWriter().println("<p><a href=\".\">Add member</a></p>");
+      resp.getWriter().println("</body></html>");
+    }
+    catch(SQLException ex)
+    {
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+      resp.getWriter().println("Error!");    
+    }
+    finally
+    {
+      if (stmt != null)
+      {
+        try
+        {
+          stmt.close();
+        }
+        catch (SQLException sqlEx)
+        {
+          //ignore;
+        }
+      }
+        if (conn != null)
+      {
+        try
+        {
+          conn.close();
+        }
+        catch (SQLException sqlEx)
+        {
+          //ignore;
+        }
+      }
+    }
   }
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,java.io.IOException
   {
